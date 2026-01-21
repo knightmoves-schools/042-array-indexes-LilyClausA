ede9432524e89c06e76ec3478c7d681110197337
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-21 20:10:49 +0000

    crftjfk

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let seasons = ['Summer', 'Fall', 'Winter', 'Spring']
 
+document.getElementById("result").innerHTML = seasons[1];
+
+
+//should create a variable named seasons that contains Summer, Fall, Winter and Spring
+//should assign the innerHTML of the HTML element with the id result to the second element in the seasons array
